6b62ee8846e4cfb38210200df23a1d0bfd7de493
melkornwah <v.chistyi@gmail.com>
2023-04-14 15:01:20 +0300

    fix: final fix for streams

--- src/App.js
+++ src/App.js
@@ -4,55 +4,55 @@ function App() {
   return (
     <div className="App">
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/MHpG2erIcgw" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/MHpG2erIcgw" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
       </div>
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/cH7VBI4QQzA" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/cH7VBI4QQzA" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
       </div>
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/h1wly909BYw" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/h1wly909BYw" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
       </div>
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/ydYDqZQpim8" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/ydYDqZQpim8" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
       </div>
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/Lc4p_XJq3jM" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/Lc4p_XJq3jM" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
       </div>
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/-9bbbhjTE20" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/-9bbbhjTE20" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
       </div>
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/1-iS7LArMPA" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/1-iS7LArMPA" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
       </div>
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/BoveYtK8Lcc" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/BoveYtK8Lcc" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
       </div>
       <div className="stream_container">
-        <iframe width="640" height="325" src="https://www.youtube.com/embed/m4f528w_Z7M" title="YouTube video player"
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/m4f528w_Z7M" title="YouTube video player"
           frameBorder="0"
           allow="autoplay; gyroscope; web-share"
         />
